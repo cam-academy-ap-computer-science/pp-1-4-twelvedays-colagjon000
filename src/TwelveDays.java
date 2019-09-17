81dc1da392d020dd04e88a4512f2f57bb3cda1fd
Jonah Colagross <colagjon000@battlegroundps.org>
2019-09-17 07:49:31 -0700

    First 2 Verses.

--- src/TwelveDays.java
+++ src/TwelveDays.java
@@ -3,7 +3,21 @@ public class TwelveDays {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		init();
+		firstDay();
+		init();
+		secondDay();
+		
+	}
+	public static void init() {
+		System.out.println("One the first day of Christmas,\nmy true love sent to me");
+	}
+	public static void firstDay() {
+		System.out.println("a partridge in a pear tree.\n");
+	}
+	public static void secondDay() {
+		System.out.println("two turtle doves, and");
+		firstDay();
 	}
 
 }
